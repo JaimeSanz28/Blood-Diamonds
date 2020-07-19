7594b1ed4ca26db0591c529766ecb4727cf0746d
Jaime Sanz <jaime@sanz.eu>
2020-07-19 17:05:08 +0200

    Edited clean.py functions

--- src/clean.py
+++ src/clean.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 def numeric(df):
     df.replace({'cut':{'Fair':1, 'Good':2, 'Very Good':3, 'Ideal':4, 'Premium':5,}}, inplace=True)
-    pass
+    df.replace({'color': {'F':1, 'E':2, 'D':3, 'H':4, 'G':5, 'J':6, 'I':7}}, inplace=True)
+    df.replace({'clarity': {'VS2':1, 'SI1':2, 'VS1':3, 'SI2':4, 'VVS2':5, 'VVS1':6, 'IF':7, 'I1':8}}, inplace=True)
+    
